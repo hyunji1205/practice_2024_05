3c5be0b291e6bd9a1fbc1b5d53bf19dff294447e
hyunji1205 <sguswl1205@gmail.com>
2024-05-17 14:32:44 +0900

    답변 조회하기 reverse

--- src/test/java/com/practice/sbb/SbbApplicationTests.java
+++ src/test/java/com/practice/sbb/SbbApplicationTests.java
@@ -1,9 +1,12 @@
 package com.practice.sbb;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,12 +22,18 @@ class SbbApplicationTests {
 	@Autowired
 	private AnswerRepository answerRepository;
 
+
+	@Transactional
 	@Test
 	void testJpa() {
-		Optional<Answer> oa = this.answerRepository.findById(1);
-		assertTrue(oa.isPresent());
-		Answer a = oa.get();
-		assertEquals(2, a.getQuestion().getId());
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 
 	}
 }
@@ -86,3 +95,8 @@ class SbbApplicationTests {
 //		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
 //		a.setCreateDate(LocalDateTime.now());
 //		this.answerRepository.save(a);
+
+//		Optional<Answer> oa = this.answerRepository.findById(1);
+//		assertTrue(oa.isPresent());
+//		Answer a = oa.get();
+//		assertEquals(2, a.getQuestion().getId());
